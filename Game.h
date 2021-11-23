4db8d1bf04dece9d708fa56960293348b92e9607
telescopelight <rngyqhs@uos.ac.kr>
2021-11-23 10:27:35 +0900

    project1

---
+++ Game.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <SDL.h>
+
+
+struct Vector2
+{
+	float x;
+	float y;
+};
+
+class Game
+{
+public:
+	Game();
+	bool Initialize();
+	void RunLoop();
+	void Shutdown();
+private:
+	void ProcessInput();
+	void UpdateGame();
+	void GenerateOutput();
+
+	SDL_Window* mWindow;
+	SDL_Renderer* mRenderer;
+	Uint32 mTicksCount;
+	bool mIsRunning;
+	
+	int mPaddleDir;
+	Vector2 mPaddlePos;
+	Vector2 mBallPos;
+	Vector2 mBallVel;
+};
